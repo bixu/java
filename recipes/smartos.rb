325c32e92e481b280cbf9ac0a3970ec6710f0d5d
Blake Irvin <blake.irvin@yetu.com>
2015-06-16 18:50:27 +0200

    WIP

--- recipes/smartos.rb
+++ recipes/smartos.rb
@@ -1,3 +1,6 @@
-package "openjdk#{node['java']['jdk_version']}" do
-	action :install
+execute "install java" do
+  command "pkgin -y install openjdk#{node['java']['jdk_version']}"
+  not_if "pkgin list | grep openjdk#{node['java']['jdk_version']}"
 end
+
+include_recipe 'java::set_java_home'
